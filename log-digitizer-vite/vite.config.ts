003ebd3ffd9261e83ec45b9e4c7b3d44f784b473
hyun1077 <skyward1088@gmail.com>
2025-09-26 18:34:51 +0900

    Update vite.config.ts

--- log-digitizer-vite/vite.config.ts
+++ log-digitizer-vite/vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// 저장소명이 Log-scale-Graph-Digitizer- 이고,
-// 앱이 log-digitizer-vite/ 폴더에 있으므로 아래처럼 설정
 export default defineConfig({
   plugins: [react()],
+  // 레포: Log-scale-Graph-Digitizer-  /  앱 폴더: log-digitizer-vite
   base: '/Log-scale-Graph-Digitizer-/log-digitizer-vite/',
 })
