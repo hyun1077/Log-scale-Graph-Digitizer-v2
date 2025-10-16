6b5cac29c946d68131315ccc58819c492fe03ff8
hyun1077 <skyward1088@gmail.com>
2025-10-16 10:16:11 +0900

    Update main.tsx

--- log-digitizer-vite/src/main.tsx
+++ log-digitizer-vite/src/main.tsx
@@ -1,10 +1,10 @@
-// src/main.tsx
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css' // 👇 이 코드가 추가되었습니다.
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
